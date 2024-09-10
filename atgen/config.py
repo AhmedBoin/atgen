91885c54e342bb90615c56bf5be73600e5d7b001
Mohamed <mohamed@Ahmeds-MacBook-Air.local>
2024-09-10 22:53:53 +0300

    add remove support

--- atgen/config.py
+++ atgen/config.py
@@ -8,7 +8,7 @@ class ATGENConfig:
     def __init__(self, default_activation=nn.ReLU(), last_activation=None, bias=True, crossover_rate = 0.8, crossover_decay_rate=1.0, 
                  last_crossover_rate=0.8, mutation_decay_rate=1.0,last_mutation_rate=0.001, weight_mutation_rate=0.1, perturbation_rate=0.9, 
                  add_neuron_mutation_rate=0.01, add_filter_mutation_rate=0.001, linear_mutation_rate=0.0001, conv_mutation_rate=0.0001, 
-                 activation_mutation_rate=0.0001, threshold=0.01, activation_dict: List[nn.Module]=activation_functions, 
+                 activation_mutation_rate=0.0001, threshold=0.01, activation_dict: List[nn.Module]=activation_functions, remove=False,
                  species_metrics=None, linear_start=True, input_size=None):
         
         self.default_activation = default_activation
@@ -21,6 +21,7 @@ class ATGENConfig:
 
         self.mutation_decay_rate = mutation_decay_rate
         self.last_mutation_rate = last_mutation_rate
+        self.remove = remove
 
         self.weight_mutation_rate = weight_mutation_rate
         self.perturbation_rate = perturbation_rate
